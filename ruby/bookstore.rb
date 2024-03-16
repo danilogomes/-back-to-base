852da1e021484a30f52653beb69fd57075ce5079
Danilo Gomes <danilomlgomes@gmail.com>
2024-03-16 18:23:17 -0300

    add class book

---
+++ ruby/bookstore.rb
@@ -0,0 +1,20 @@
+class Book
+  
+  def initialize(author, isbn= 1, pages_number, price)
+    @author = author
+    @isbn = isbn 
+    @pages_number = pages_number
+    @price = price
+  end
+
+  #def output_book
+  def to_s
+    "Author: #{@author} | ISBN: #{@isbn} | Pages Number: #{@pages_number} | Price: #{@price}"
+  end
+end
+
+c_plus_plus_cookbook = Book.new("Maurício Gomes", "1234567", 247, 60.9)
+delphi_cookbook = Book.new("Marco Sangali", "7654321", 378, 60.9)
+
+puts c_plus_plus_cookbook
+puts delphi_cookbook
